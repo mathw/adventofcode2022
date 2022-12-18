060b7840e89ad9f207906fe7bca44028386d6a66
Matthew Walton <matthew@mathw.me.uk>
2022-12-18 08:54:15 +0000

    Day 18 Part 2 Doesn't Work Yet
    Passes all the tests I've thought of so far, but the real input seems to be considering something to be exposed that the site thinks is not.

--- src/day18.rs
+++ src/day18.rs
@@ -5,7 +5,11 @@ pub fn run() -> day::Result {
     let input = include_str!("inputs/day18.txt");
     let blob = Blob::from_str(input)?;
     let exposed_faces = blob.count_exposed_faces();
-    Ok((Some(format!("{} exposed faces", exposed_faces)), None))
+    let exposed_surface_area = blob.external_surface_area();
+    Ok((
+        Some(format!("{} exposed faces", exposed_faces)),
+        Some(format!("Exposed surface area {}", exposed_surface_area)),
+    ))
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -69,6 +73,26 @@ impl Blob {
 
         exposed_faces
     }
+
+    fn exposed_face_cubes(&self) -> impl Iterator<Item = Cube> + '_ {
+        self.cubes.iter().flat_map(|c| {
+            c.surrounding()
+                .into_iter()
+                .filter(|sc| !self.cubes.contains(sc))
+        })
+    }
+
+    fn external_surface_area(&self) -> usize {
+        let exposed_cubes: Vec<Cube> = self.exposed_face_cubes().collect();
+        exposed_cubes
+            .iter()
+            .filter(|c| {
+                c.surrounding()
+                    .into_iter()
+                    .any(|sc| !self.cubes.contains(&sc) && !exposed_cubes.contains(&sc))
+            })
+            .count()
+    }
 }
 
 impl FromStr for Blob {
@@ -125,4 +149,186 @@ const INPUT: &str = "2,2,2
 fn test_part1_full_sample() {
     let blob = Blob::from_str(INPUT).expect("This should parse");
     assert_eq!(blob.count_exposed_faces(), 64);
+}
+
+#[test]
+fn test_part2_full_sample() {
+    let blob = Blob::from_str(INPUT).expect("This should parse");
+    assert_eq!(blob.external_surface_area(), 58);
+}
+
+#[test]
+fn test_part2_small_sample() {
+    let blob = Blob::from_str(
+        "1,1,1
+2,1,1",
+    )
+    .expect("This should parse");
+    assert_eq!(blob.external_surface_area(), 10);
+}
+
+#[test]
+fn test_part2_hollow_sample() {
+    let blob = Blob::from_str(
+        "1,1,1
+3,1,1
+2,1,2
+2,1,0
+2,2,1
+2,0,1",
+    )
+    .expect("This should parse");
+    assert_eq!(blob.count_exposed_faces(), 36);
+    assert_eq!(blob.external_surface_area(), 30);
+}
+
+#[test]
+fn test_part2_hollow_sample_2() {
+    let blob = Blob::from_str(
+        "0,1,1
+1,1,1
+3,1,1
+2,1,2
+2,1,0
+2,2,1
+2,0,1",
+    )
+    .expect("This should parse");
+    assert_eq!(blob.count_exposed_faces(), 40);
+    assert_eq!(blob.external_surface_area(), 34);
+}
+
+#[test]
+fn test_part2_hollow_cube_sample() {
+    let blob = Blob::from_str(
+        "0,0,0
+1,0,0
+2,0,0
+0,0,1
+0,0,2
+1,0,1
+1,0,2
+2,0,1
+2,0,2
+0,2,0
+1,2,0
+2,2,0
+0,2,1
+0,2,2
+1,2,1
+1,2,2
+2,2,1
+2,2,2
+0,1,0
+1,1,0
+2,1,0
+0,1,1
+0,1,2
+1,1,2
+2,1,1
+2,1,2",
+    )
+    .expect("This should parse");
+    assert_eq!(blob.count_exposed_faces(), 60);
+    assert_eq!(blob.external_surface_area(), 54);
+}
+
+#[test]
+fn test_part2_big_hollow_cube_containing_cube() {
+    let blob = Blob::from_str(
+        "0,0,0
+1,0,0
+2,0,0
+3,0,0
+4,0,0
+0,0,1
+0,0,2
+0,0,3
+0,0,4
+1,0,1
+1,0,2
+1,0,3
+1,0,4
+2,0,1
+2,0,2
+2,0,3
+2,0,4,
+3,0,1
+3,0,2
+3,0,3
+3,0,4
+0,4,0
+1,4,0
+2,4,0
+3,4,0
+4,4,0
+0,4,1
+0,4,2
+0,4,3
+0,4,4
+1,4,1
+1,4,2
+1,4,3
+1,4,4
+2,4,1
+2,4,2
+2,4,3
+2,4,4,
+3,4,1
+3,4,2
+3,4,3
+3,4,4
+0,1,0
+1,1,0
+2,1,0
+3,1,0
+4,1,0
+0,1,1
+0,1,2
+0,1,3
+0,1,4
+1,1,4
+2,1,4
+3,1,4
+4,1,4
+4,1,3
+4,1,2
+4,1,1
+0,2,0
+1,2,0
+2,2,0
+3,2,0
+4,2,0
+0,2,1
+0,2,2
+0,2,3
+0,2,4
+1,2,4
+2,2,4
+3,2,4
+4,2,4
+4,2,3
+4,2,2
+4,2,1
+0,3,0
+1,3,0
+2,3,0
+3,3,0
+4,3,0
+0,3,1
+0,3,2
+0,3,3
+0,3,4
+1,3,4
+2,3,4
+3,3,4
+4,3,4
+4,3,3
+4,3,2
+4,3,1
+2,2,2",
+    )
+    .expect("This should parse");
+    assert_eq!(blob.count_exposed_faces(), 150 + (9 * 6) + 6);
+    assert_eq!(blob.external_surface_area(), 150);
 }
